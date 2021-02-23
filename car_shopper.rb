0770058ab70d20c1714355188d8c06fe650555ae
barabas007 <shop.ru.shop@mail.ru>
2021-02-23 18:48:28 +0300

    converting Price to a Number

--- car_shopper.rb
+++ car_shopper.rb
@@ -2,6 +2,12 @@ require 'httparty'
 require 'nokogiri'
 require 'pp'
 
+def convert_price(price)
+  price.gsub('$', '').gsub(',','').to_i  
+end
+
+
+
 def car_shopper
   response = HTTParty.get('https://code.evgenyrahman.com/rubycourse/carlist.html')
   parsed_html = Nokogiri::HTML(response.body)
@@ -21,9 +27,9 @@ def car_shopper
 cars = car_listings.map do |each_car|
    {
       make: each_car.css('.make').text, 
-      year: each_car.css('.year').text,
-      price: each_car.css('.price').text,
-      rating: each_car.css('.star.rating').attribute("data-rating").value 
+      year: each_car.css('.year').text.to_i,
+      price: convert_price(each_car.css('.price').text),
+      rating: each_car.css('.star.rating').attribute("data-rating").value.to_i
     }
 end
 
@@ -32,7 +38,7 @@ end
   File.open("car_listings.json", "wb") do |f|
     f << cars.to_json 
   end
-
+  puts 'Result were written to file !'
 
   #puts 'Welcome to the shopper!'
 
